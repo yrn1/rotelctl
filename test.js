e805621cc4f435c6d46c83c65454cd92309190f2
Jeroen Baekelandt <yrn001@gmail.com>
2014-09-23 15:51:07 +0200

    all sized getters

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
+"use strict";
 var rotelctl = require('./lib/rotelctl');
+var async = require('async');
 
 var rctl = new rotelctl.RotelCtl('/dev/ttyUSB0');
 rctl.open(function(err) {
   if (err) {
     return console.error(err);
   }
-  rctl.getDisplay('get_display!', function(err, data) {
+  async.parallel({
+    'getDisplay': rctl.getDisplay.bind(rctl),
+    'getDisplay1': rctl.getDisplay1.bind(rctl),
+    'getDisplay2': rctl.getDisplay2.bind(rctl),
+    'getProductType': rctl.getProductType.bind(rctl),
+    'getProductVersion': rctl.getProductVersion.bind(rctl),
+    'getTcVersion': rctl.getTcVersion.bind(rctl)
+  }, function(err, results) {
     if (err) {
-      return console.error(err);
+      console.error(err);
+    } else {
+      console.log(results);
     }
-    console.log('"' + data + '"');
     rctl.close(function(err) {
       if (err) {
         return console.error(err);
